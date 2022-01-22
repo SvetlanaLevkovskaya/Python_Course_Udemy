2ddd062f827e0dbf1abbb64d5a3c65323b4a907d
Svetlana Levkovskaya <93194364+SvetlanaLevkovskaya@users.noreply.github.com>
2022-01-22 21:34:36 +0300

    Python_Course_Udemy
    Python_Course_Udemy

--- day_04.py
+++ day_04.py
@@ -14,3 +14,59 @@ print(random_float_from_1_to_5)
 love_score = random.randint(1, 100)
 print(f"You love score is {love_score}")
 
+print("Lists+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
+                     "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island",
+                     "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois",
+                     "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin",
+                     "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado",
+                     "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma",
+                     "New Mexico", "Arizona", "Alaska", "Hawaii"]
+print("************************************************************************************")
+print("                                                                                     ")
+print(states_of_america)
+print(states_of_america[0])
+
+name = states_of_america[-1]
+print("************************************************************************************")
+print("                                                                                     ")
+states_of_america[0] = "Minsk"
+print(states_of_america)
+
+print("************************************************************************************")
+print("                                                                                     ")
+states_of_america.append("Belarus")
+print(states_of_america)
+print("************************************************************************************")
+print("                                                                                     ")
+states_of_america.extend(["skdjhfkjshfkj", "ksjfhkjhsfk"])
+print(states_of_america)
+
+print(len(states_of_america))
+print("************************************************************************************")
+print("                                                                                     ")
+
+a = ['one', 'eleven', 'twelve', 'five', 'six', 'ten']
+
+print(a)
+for i in range(5):
+    print(random.choice(a))
+
+print("************************************************************************************")
+print("                                                                                     ")
+num_of_states = len(states_of_america)
+print(num_of_states)
+print(states_of_america[53-1])
+print("************************************************************************************")
+print("                                                                                     ")
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries",
+               "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen1 = [fruits, vegetables]
+print(dirty_dozen1[1])
+
+
---
+++ day_04_task_2_Banker_Roulette.py
@@ -0,0 +1,24 @@
+import random
+
+'''You are going to write a program that will select a random name from a list of names. The person selected will 
+have to pay for everybody's food bill.
+
+Important: You are not allowed to use the choice() function.
+
+Line 8 splits the string names_string into individual names and puts them inside a List called names. For this to work, 
+you must enter all the names as names followed by comma then space. e.g. name, name, name
+
+When you run the code, just use a random number as the seed. e.g. 67346 It doesn't matter what you chose, it's only for 
+our testing code to check your work.'''
+
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# print(names)
+length_of_list = len(names)
+random_choice = random.randint(0, length_of_list-1)
+random_name = names[random_choice]
+# random_names = random.choice(names)
+print(f"{random_name} is going to buy the meal today!")
