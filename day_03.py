2e1e23c1e79ba540eebf4871ab67ff95a8e1c7e1
Svetlana Levkovskaya <93194364+SvetlanaLevkovskaya@users.noreply.github.com>
2022-01-22 17:41:10 +0300

    Python_Course_Udemy
    Python_Course_Udemy

--- day_03.py
+++ day_03.py
@@ -6,11 +6,11 @@
 # else:
 #     print("Sorry, you have to grow taller before you can ride.")
 
-print("Even or Odd ++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("Even or Odd +++++++++++++++++++++++++++++++++++++++++++++++++++")
 print("2 % 2 = 0 # even")
 print("3 % 2 = 1 # odd")
 
-print("Nested if statements and elif statement++++++++++++++++++++++++++++")
+print("Nested if statements and elif statement+++++++++++++++++++++++++")
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
@@ -27,7 +27,7 @@ else:
     print("Sorry, you have to grow taller before you can ride.")
 
 
-print("Multiple if statements in Succession ++++++++++++++++++++++++++++")
+print("Multiple if statements in Succession +++++++++++++++++++++++++++")
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 bill = 0
@@ -53,3 +53,42 @@ if height >= 120:
 
 else:
     print("Sorry, you have to grow taller before you can ride.")
+
+
+print("Logical Operators++++++++++++++++++++++++++++++++++++++++++++++++")
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+    print("You can ride the rollercoster!")
+    age = int(input("What's is your age? "))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5. ")
+    elif age <= 18:
+        bill = 12
+        print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
+
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("Angela".lower())
+print("Angela".count("a"))
+print("Angela".lower().count("a"))
+
+lower_case_name = "Angela".lower()
+print(lower_case_name.count("a"))
---
+++ day_03_task_5_Love_Calculator.py
@@ -0,0 +1,41 @@
+"""You are going to write a program that tests the compatibility between two people.
+
+To work out the love score between two people:
+
+Take both people's names and check for the number of times the letters in the word TRUE occurs.
+
+Then check for the number of times the letters in the word LOVE occurs.
+
+Then combine these numbers to make a 2 digit number."""
+
+
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+check_T = (name1 + name2).upper().count('T')
+check_R = (name1 + name2).upper().count('R')
+check_U = (name1 + name2).upper().count('U')
+check_E = (name1 + name2).upper().count('E')
+check_TRUE = int(check_T+check_R+check_U+check_E)
+# print(check_TRUE)
+check_L = (name1 + name2).upper().count('L')
+check_O = (name1 + name2).upper().count('O')
+check_V = (name1 + name2).upper().count('V')
+check_E = (name1 + name2).upper().count('E')
+check_LOVE = int(check_L+check_O+check_V+check_E)
+# print(check_LOVE)
+love_score = str(check_TRUE) + str(check_LOVE)
+print(type(love_score))
+
+if int(love_score) < 10 or int(love_score) > 90:
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif int(love_score) > 40 and int(love_score) < 50:
+    print(f"Your score is {love_score}, you are alright together.")
+else:
+    print(f"Your score is {love_score}.")
+
+
+# David Beckham = 4
+# Viktoria Beckham  = 5
+# total = 45
