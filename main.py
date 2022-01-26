af22ab3b70584d5bd743359a6ac5f65c2a83ea63
Svetlana Levkovskaya <93194364+SvetlanaLevkovskaya@users.noreply.github.com>
2022-01-26 17:27:39 +0300

    Python_Course_Udemy
    Python_Course_Udemy

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+for i, j in enumerate(['foo', 'bar', 'baz']):
+    if j == 'bar':
+        print(i)
 
+mylist = ["foo", "bar", "baz", "bar"]
+newlist = enumerate(mylist)
+for index, item in newlist:
+    if item == "bar":
+        print(index, item)
